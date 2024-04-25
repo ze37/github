718ea55d68c9bc08b567b9fae278ef59bcfada8a
ze37 <josealvess6154@gmail.com>
2024-04-25 20:25:03 -0300

    Add html commet template string

---
+++ matching-game2/src/Objects/ScoreBoard/index.js
@@ -0,0 +1,11 @@
+import PlayerName from "../../components/PlayerName"
+function ScoreBoard(){
+    return /*html*/`
+        <header class="score-board">
+            ${PlayerName("Player1")}
+            ${PlayerName("Player2")}
+        </header>
+    `
+}
+
+export default ScoreBoard

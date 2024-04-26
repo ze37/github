b3f31ad1a018d983f008fff52b18a9591d2785fb
ze37 <josealvess6154@gmail.com>
2024-04-26 06:34:58 -0300

    Add the CardGame with javascript icon

--- matching-game2/src/components/CardFrontBack/index.js
+++ matching-game2/src/components/CardFrontBack/index.js
@@ -4,7 +4,7 @@ function CardFrontBack(){
     return /*html*/`
         <article class="card-front-back">
             ${CardeGame()}
-            ${CardeGame()}
+            ${CardeGame("javascript","Logo do javascript")}
         </article>
 
     `

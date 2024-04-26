41722451a7df439f9eb6c1eace5bccea33407658
ze37 <josealvess6154@gmail.com>
2024-04-26 20:40:04 -0300

    Add event in CardFrontBack component

--- matching-game2/src/components/CardFrontBack/index.js
+++ matching-game2/src/components/CardFrontBack/index.js
@@ -1,8 +1,16 @@
 import CardeGame from "../CardGame"
 import "./style.css"
+
 function CardFrontBack(){
+    window.cardFrontBack = {}
+    window.cardFrontBack.handleClick = (event)=>{
+        const $origin = event.target
+        const $cardFrontBack = $origin.closest(".card-front-back")
+         
+        $cardFrontBack.classList.toggle("-active")
+    }
     return /*html*/`
-        <article class="card-front-back">
+        <article class="card-front-back" onclick="cardFrontBack.handleClick(event)">
             <div class="card -front">
                 ${CardeGame()}
             </div>

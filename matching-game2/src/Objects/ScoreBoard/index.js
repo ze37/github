3e391ea0593a0af71165d3c23d7c38a3189dae16
ze37 <josealvess6154@gmail.com>
2024-04-27 13:50:11 -0300

    Create HTML of the PlayerScore

--- matching-game2/src/Objects/ScoreBoard/index.js
+++ matching-game2/src/Objects/ScoreBoard/index.js
@@ -1,11 +1,14 @@
 import "./style.css"
 import PlayerName from "../../components/PlayerName"
 import VsPlayer from "../../components/VsPlayer"
+import PlayerScore from "../../components/PlayerScore"
 function ScoreBoard(){
     return /*html*/`
         <header class="score-board">
             ${PlayerName("Player1")}
+            ${PlayerScore(1)}
             ${VsPlayer()}
+            ${PlayerScore()}
             ${PlayerName("Player2")}
         </header>
     `
---
+++ matching-game2/src/components/PlayerScore/index.js
@@ -0,0 +1,13 @@
+function PlayerScore (points = 0){
+    return /*html*/`
+        <ol data-points="${points}">
+            <li>Um</li>
+            <li>Dois</li>
+            <li>Tres</li>
+        </ol>
+    `
+
+    
+}
+
+export default PlayerScore

93d335bddd5d3e5f74fe4e162e3a17c1e2b75151
ze37 <josealvess6154@gmail.com>
2024-04-29 13:50:32 -0300

    Modificado o arquivo App.js

--- JogoDaVelha/jogo-da-velha/src/App.js
+++ JogoDaVelha/jogo-da-velha/src/App.js
@@ -3,11 +3,11 @@ import Sobre from "./object/Sobre"
 import Logo from "./object/Logo"
 function App() {
   return (
-    <div>
+    <>
       <Logo />
       <Sobre />
-    </div>
-    
+    </>
+  
   )
 }
 

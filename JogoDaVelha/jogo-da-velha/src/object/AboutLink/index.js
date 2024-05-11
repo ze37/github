8746c260bf982c9df461b998092640028a906299
ze37 <josealvess6154@gmail.com>
2024-05-11 18:56:12 -0300

    Refactory trocamos o nome da class sobre para o aboutlink

--- JogoDaVelha/jogo-da-velha/src/object/AboutLink/index.js
+++ JogoDaVelha/jogo-da-velha/src/object/AboutLink/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import './style.css'
 const Sobre = ()=>{
 
-    return(<a href="#to-do" className="sobre">Sobre</a>)
+    return(<a href="#to-do" className="aboutlink">Sobre</a>)
 }
 
 export default Sobre

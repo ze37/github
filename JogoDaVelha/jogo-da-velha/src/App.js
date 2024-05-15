3c7839db4a90e76a8c87af49faf3021cf87204e5
ze37 <josealvess6154@gmail.com>
2024-05-15 06:15:25 -0300

    Criamos o object Avatar_Profile

--- JogoDaVelha/jogo-da-velha/src/App.js
+++ JogoDaVelha/jogo-da-velha/src/App.js
@@ -3,9 +3,11 @@ import "./App.css"
 
 import Checkbox from "./object/Checkbox";
 import About from "./object/About"
+import AvatarProfile from "./object/AvatarProfile";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
 import HeaderInternal from "./components/HeaderInternal";
+import profile from "./object/img/zeneto.jpg"
 
 const App = ()=> (
     <main className="app">
@@ -14,6 +16,7 @@ const App = ()=> (
       <Checkbox id="show" value="show" type="checkbox" content="Mostrar eventos"/>
       <About>
         <HeaderInternal />
+        <AvatarProfile src={profile} alt="Avatar do zeneto"/>
       </About>
     </main> 
 )

5ce4ef40c0ea253cd6ca51272c2e81c47056c016
ze37 <josealvess6154@gmail.com>
2024-05-15 14:16:10 -0300

    Criamos o CSS do profileUser

--- JogoDaVelha/jogo-da-velha/src/components/ProfileUser/index.js
+++ JogoDaVelha/jogo-da-velha/src/components/ProfileUser/index.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./style.css"
 import profile from "../../object/img/zeneto.jpg"
 import AvatarProfile from "../../object/AvatarProfile";
 const ProfileUser = ()=>(
-    <dl>
-        <dd>
+    <dl className="profile-user">
+        <dd className="avatar">
             <AvatarProfile src={profile} alt="Avatar do zeneto"/>
         </dd>
-            <dt>Ze Alves</dt>
-            <dd>Migrei de ajudante de predeiro pra dev</dd>
+            <dt className="title">Ze Alves</dt>
+            <dd className="description">Migrei de ajudante de predeiro pra dev</dd>
     </dl>
 )
 

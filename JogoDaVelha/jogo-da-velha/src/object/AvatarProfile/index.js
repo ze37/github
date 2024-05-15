cba6682d3e92fb8a3d5f5d36102096a273ec703e
ze37 <josealvess6154@gmail.com>
2024-05-15 12:57:14 -0300

    Fizemos o CSS do object AvatarProfile

--- JogoDaVelha/jogo-da-velha/src/object/AvatarProfile/index.js
+++ JogoDaVelha/jogo-da-velha/src/object/AvatarProfile/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-
+import "./style.css"
 const AvatarProfile = ({src,alt})=>(
-    <img src={src} alt={alt} />
+    <img className="avatar-profile" src={src} alt={alt} />
 );
 
 export default AvatarProfile

173e17dce079cd7fcd9e89c8b794516402b2831e
ze37 <josealvess6154@gmail.com>
2024-05-17 18:33:04 -0300

    Foi removido alguns arquivos desnessesarios

--- projeto-react/src/App.jsx
+++ projeto-react/src/App.jsx
@@ -1,17 +1,21 @@
 import { Pessoa } from "./assets/Pessoa";
 import { SeyMyName } from "./assets/SeyMyName";
-
+import { List } from "./assets/List";
 
 export function App() {
-
-return (
-  <main>
-    <h1>Todo app</h1>
-    <input type="text" />
-    <button>Add</button>
-    <SeyMyName nome="zeneto" />
-    <Pessoa nome="zeneto" idade="40" Progamador="Progamador" foto="https://via.placeholder.com/150"/>
-  </main>  
-)
+  return (
+    <main>
+      <h1>Todo app</h1>
+      <input type="text" />
+      <button>Add</button>
+      <SeyMyName nome="zeneto" />
+      <Pessoa
+        nome="zeneto"
+        idade="40"
+        Progamador="Progamador"
+        foto="https://via.placeholder.com/150"
+      />
+      <List />
+    </main>
+  );
 }
-
--- projeto-react/src/assets/Pessoa.jsx
+++ projeto-react/src/assets/Pessoa.jsx
@@ -1,9 +1,9 @@
-import "./pessoa.css"
-export const Pessoa = ({nome,idade,foto,Progamador})=>(
-    <div className="pessoa">
-        <img src={foto} alt="pessoa" />
-        <h1>Nome: {nome}</h1>
-        <p>Idade : {idade}</p>
-        <p>Proficao : {Progamador}</p>
-    </div>
-)
+import "./pessoa.css";
+export const Pessoa = ({ nome, idade, foto, Progamador }) => (
+  <div className="pessoa">
+    <img src={foto} alt="pessoa" />
+    <h1>Nome: {nome}</h1>
+    <p>Idade : {idade}</p>
+    <p>Proficao : {Progamador}</p>
+  </div>
+);

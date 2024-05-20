688c7949ee5a38fc09e087de8785eabbe2bddc16
ze37 <josealvess6154@gmail.com>
2024-05-19 23:09:21 -0300

    Criando components com props e children

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,10 +1,16 @@
-import Footer from "./components/footer";
+import Banner from "./components/Banner";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Banner />
+      <Container>
+        <h1>Ola mundo</h1>
+      </Container>
       <Footer />
     </>
   );

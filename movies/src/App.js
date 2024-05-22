c7db474b4059de87be8937c6d52abccb615cc459
ze37 <josealvess6154@gmail.com>
2024-05-22 12:18:24 -0300

    Criado o component Category

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,21 +1,27 @@
 import Banner from "./components/Banner";
 import Card from "./components/Cards";
+import Category from "./components/Category";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import videos from "./json/db.json"
-
+import videos from "./json/videos.json";
 function App() {
   return (
     <>
       <Header />
       <Banner image="favoritos" />
       <Container>
-        <h2>Geografia</h2>
+        {/* <h2>Geografia</h2>
         <section className="cards">
-          {videos.map((video) =><Card id={video.id} key={video.id} />)}
-        </section>
-
+          {videos.map((video) => (
+            <Card id={video.id} key={video.id} />
+          ))}
+        </section> */}
+        <Category category="Geogafia">
+          {videos.map((video) => (
+            <Card id={video.id} key={video.id} />
+          ))}
+        </Category>
       </Container>
       <Footer />
     </>
---
+++ movies/src/components/Category/index.js
@@ -0,0 +1,11 @@
+import "./category.css";
+function Category({ category, children }) {
+  return (
+    <section className="category">
+      <h2>{category}</h2>
+      <div>{children}</div>
+    </section>
+  );
+}
+
+export default Category;

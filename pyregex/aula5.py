8a9670387e9fe7fc90d6d8de6504fa0e5a79c116
ze37 <josealvess6154@gmail.com>
2024-05-28 18:22:04 -0300

    Criou o arquivo aula5.py e usando grupos e retrovissores

---
+++ pyregex/aula5.py
@@ -0,0 +1,16 @@
+# Meta Caracteres ^ $
+
+# [@#a-zA-Z0-9] isso é um conjunto
+# (abc | def) isso é um grupo
+
+import re
+from pprint import pprint
+texto = '''
+<p>fraze 1</p><p>fraze 2</p><p>fraze 3</p><div>fraze 4</div>
+'''
+tags = re.findall(r'<([pdiv]{1,3})>(?:.*?)<\/\1>',texto)
+pprint(tags)
+
+#for tag in tags:
+ #   um, dois, tres= tag
+  #  print(tres)

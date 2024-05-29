97ccb26fed3b08bcd5f2ec9815233cc83a920c0b
ze37 <josealvess6154@gmail.com>
2024-05-29 18:13:01 -0300

    Criado o arquivo aula7.py

---
+++ pyregex/aula7.py
@@ -0,0 +1,16 @@
+import re
+
+
+texto = '''
+João trouxe    flores para sua amada namorada em 10 de janeiro de 1970,
+Maria era o nome dela.
+
+Foi um ano exelente na vida de joao. Teve 5 filhos, todos adultos atualmente.
+maria, hoje sua esposa, ainda faz aquele cafe com paode queijo nas tardes de
+domingo. Tambemné! Sendo a boa mineira que é, nunca esquece seu famos
+pao de queijo.
+Nao canso de ouvir a Maria:
+"Jooooooooooãoooo o cafe ta protinho aqui. veeeemmm"
+
+'''
+print(re.findall(r'[a-z]+', texto))

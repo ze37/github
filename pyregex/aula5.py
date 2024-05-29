17f23cc1c32653271cd91050d7f54f5f303d29aa
ze37 <josealvess6154@gmail.com>
2024-05-29 06:28:54 -0300

    Validando cpf

--- pyregex/aula5.py
+++ pyregex/aula5.py
@@ -8,8 +8,11 @@ from pprint import pprint
 texto = '''
 <p>fraze 1</p><p>fraze 2</p><p>fraze 3</p><div>fraze 4</div>
 '''
-tags = re.findall(r'<([pdiv]{1,3})>(?:.*?)<\/\1>',texto)
-pprint(tags)
+cpf = '134.547.362-32'
+
+pprint(re.findall(r'[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}',cpf))
+#tags = re.findall(r'<([pdiv]{1,3})>(?:.*?)<\/\1>',texto)
+#pprint(tags)
 
 #for tag in tags:
  #   um, dois, tres= tag

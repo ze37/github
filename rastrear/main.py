656cc77efc68679aa572d113058742792489e724
ze37 <josealvess6154@gmail.com>
2024-06-04 12:48:03 -0300

    adicionado o butao

--- rastrear/main.py
+++ rastrear/main.py
@@ -14,26 +14,29 @@ root = Tk()
 root.title("Rastreamento De Numeros")
 root.geometry("365x584")
 root.resizable(False,False)
-
+# Crregar o logo do sistema
 logo = PhotoImage(file="rastro1.png")
 Label(root,image=logo).place(x=240,y=70)
 # colocando icon no formulario
-icon = Image.open("rastro.png")
+icon = Image.open("rastro1.png")
 photo = ImageTk.PhotoImage(icon)
 root.wm_iconphoto(False,photo)
-
+# texto ao lado do icon
 cabecalho = Label(root,text="Rastreamento De Telefone", font=("arial",10,"bold"))
 cabecalho.place(x=70,y=115)
-
+#imagem do icon de pesquisa
 entry_cabecalho = PhotoImage(file="pesquiza.png")
 Label(root,image=entry_cabecalho).place(x=70,y=245)
 
-
+#campo de pesquisa
 entry = StringVar()
 enter_numero = Entry(root,textvariable=entry,width=15,bd=0,font=("arial",20),justify="center")
 enter_numero.place(x=84,y=270)
 
 
+pesquisar_image = PhotoImage(file="buttom2.png")
+pesquisar = Button(image=pesquisar_image,borderwidth=0,cursor="hand2",bd=0,font=("arial",15))
+pesquisar.place(x=64,y=305)
 
 
 

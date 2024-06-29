c51c881654442292d196950f8eefd5d9de3d5249
ze37 <josealvess@gmail.com>
2024-06-29 20:41:31 -0300

    Integrando o banco de dados ao FastApi

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,29 +1,57 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
-
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy import  select
+from sqlalchemy.orm import Session
+from fast_zero.database import get_session
+from fast_zero.models import User
 from fast_zero.schemas import Message, UserDb, UserList, UserPublic, UserSchema
-
 app = FastAPI()
 
 database = []
 
 
 @app.get('/', status_code=200, response_model=Message)
-def read_root():
+def read_root(session: Session = Depends(get_session)):
     return {'message': 'Olá Mundo!'}
 
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    user_with_id = UserDb(**user.model_dump(), id=len(database) + 1)
-    database.append(user_with_id)
-    return user_with_id
+
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username alredy exists',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email alredy exists',
+            )
+    db_user = User(
+        username=user.username, email=user.email, password=user.password
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.get('/users/', response_model=UserList)
-def read_users():
-    return {'users': database}
+def read_users(session:Session=Depends(get_session)):
+
+    user = session.scalars(
+             select(User)
+            )
+    return {'users': user}
 
 
 @app.put('/users/{user_id}', response_model=UserPublic)
--- fast_zero/tests/conftest.py
+++ fast_zero/tests/conftest.py
@@ -1,20 +1,31 @@
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
+from sqlalchemy.pool import StaticPool
 from sqlalchemy.orm import Session
 
 from fast_zero.app import app
+from fast_zero.database import get_session
 from fast_zero.models import table_registry
 
 
 @pytest.fixture()
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+        
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture()
 def session():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine('sqlite:///:memory:',
+                           connect_args={'check_same_thread': False},
+                           poolclass=StaticPool,
+                           )
     table_registry.metadata.create_all(engine)
     with Session(engine) as session:
         yield session

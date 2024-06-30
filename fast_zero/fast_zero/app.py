c76cea67df0fe5291d7553e07c76a4a8c58e4364
ze37 <josealvess@gmail.com>
2024-06-30 13:52:41 -0300

    Atualizando o endpoints para usar o banco de dados real

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,14 +1,14 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException, Depends
-from sqlalchemy import  select
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
 from sqlalchemy.orm import Session
+
 from fast_zero.database import get_session
 from fast_zero.models import User
-from fast_zero.schemas import Message, UserDb, UserList, UserPublic, UserSchema
-app = FastAPI()
+from fast_zero.schemas import Message, UserList, UserPublic, UserSchema
 
-database = []
+app = FastAPI()
 
 
 @app.get('/', status_code=200, response_model=Message)
@@ -17,8 +17,7 @@ def read_root(session: Session = Depends(get_session)):
 
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-
+def create_user(user: UserSchema, session=Depends(get_session)):
     db_user = session.scalar(
         select(User).where(
             (User.username == user.username) | (User.email == user.email)
@@ -46,30 +45,38 @@ def create_user(user: UserSchema):
 
 
 @app.get('/users/', response_model=UserList)
-def read_users(session:Session=Depends(get_session)):
-
-    user = session.scalars(
-             select(User)
-            )
+def read_users(
+    limit: int = 10, skip: int = 0, session: Session = Depends(get_session)
+):
+    user = session.scalars(select(User).limit(limit).offset(skip))
     return {'users': user}
 
 
 @app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(user_id: int, user: UserSchema):
-    if user_id > len(database) or user_id < 1:
+def update_user(
+    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+    if not db_user:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
-    user_with_id = UserDb(**user.model_dump(), id=user_id)
-    database[user_id - 1] = user_with_id
-    return user_with_id
+    db_user.email = user.email
+    db_user.username = user.username
+    db_user.password = user.password
+
+    session.commit()
+    session.refresh(db_user)
+    return db_user
 
 
 @app.delete('/users/{user_id}', response_model=Message)
-def delete_user(user_id: int):
-    if user_id > len(database) or user_id < 1:
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+    if not db_user:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
-    del database[user_id - 1]
-    return {'message': 'User delete'}
+    session.delete(db_user)
+    session.commit()
+    return {'message': 'User deleted'}
--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
@@ -31,18 +33,17 @@ def test_create_user(client):
 def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'testusername',
-                'email': 'test@gmail.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -59,6 +60,6 @@ def test_update_user(client):
     }
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('users/1')
-    assert response.json() == {'message': 'User delete'}
+    assert response.json() == {'message': 'User deleted'}
